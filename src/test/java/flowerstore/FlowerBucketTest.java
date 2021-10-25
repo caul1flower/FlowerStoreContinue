737d434d2134444c20d5cc6c6db65a0822b4c2e5
caul1flower <flame.anita@gmail.com>
2021-10-25 23:29:07 +0300

    Finished tests

--- src/test/java/flowerstore/FlowerBucketTest.java
+++ src/test/java/flowerstore/FlowerBucketTest.java
@@ -3,20 +3,42 @@ package flowerstore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlowerBucketTest {
+    private FlowerPack pack1, pack2, pack3;
+    private FlowerBucket bucket;
 
     @BeforeEach
     void setUp() {
+        Flower flower1, flower2, flower3;
 
-    }
+        flower1 = new Flower(FlowerType.CHAMOMILE);
+        flower1.setPrice(10);
+        flower2 = new Flower(FlowerType.TULIP);
+        flower2.setPrice(20);
+        flower3 = new Flower(FlowerType.ROSE);
+        flower3.setPrice(30);
 
-    @Test
-    void addFlowerPack() {
+        pack1 = new FlowerPack(flower1, 50);
+        pack2 = new FlowerPack(flower2, 100);
+        pack3 = new FlowerPack(flower3, 150);
+
+        bucket = new FlowerBucket();
     }
 
     @Test
     void getPrice() {
+        assertEquals(0.0, bucket.getPrice());
+        bucket.addFlowerPack(pack1);
+        assertEquals(500.0, bucket.getPrice());
+        bucket.addFlowerPack(pack2);
+        assertEquals(2500.0, bucket.getPrice());
+        bucket.addFlowerPack(pack3);
+        assertEquals(7000.0, bucket.getPrice());
     }
 }
--- src/test/java/flowerstore/StoreTest.java
+++ src/test/java/flowerstore/StoreTest.java
@@ -3,19 +3,48 @@ package flowerstore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 class StoreTest {
+    private Store store;
 
     @BeforeEach
     void setUp() {
+        Flower chamomile = new Flower(FlowerType.TULIP);
+        chamomile.setPrice(10);
+        chamomile.setColor(new int[]{127, 20, 30});
+        chamomile.setSepalLength(10);
+        FlowerPack packChamomile = new FlowerPack(chamomile, 10);
+
+        Flower rose = new Flower(FlowerType.ROSE);
+        rose.setPrice(50);
+        rose.setColor(new int[]{127, 10, 20});
+        rose.setSepalLength(50);
+        FlowerPack packRose = new FlowerPack(rose, 10);
+
+        Flower tulip = new Flower(FlowerType.TULIP);
+        tulip.setPrice(50);
+        tulip.setColor(new int[]{127, 10, 20});
+        tulip.setSepalLength(50);
+        FlowerPack packTulip = new FlowerPack(tulip, 10);
+
+        FlowerBucket flowerBucket = new FlowerBucket();
+        flowerBucket.addFlowerPack(packChamomile);
+        flowerBucket.addFlowerPack(packRose);
+        flowerBucket.addFlowerPack(packTulip);
+        store = new Store();
+
+        List<FlowerBucket> buckets = new ArrayList<>();
+        buckets.add(flowerBucket);
+        store.setFlowerBuckets(buckets);
     }
+
 
     @Test
     void search() {
-    }
-
-    @Test
-    void setFlowerBuckets() {
+        assertFalse(store.search(1000));
+        assertTrue(store.search(5001));
     }
 }
